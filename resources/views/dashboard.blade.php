a62a451aee6f6d2dcbd19340cbc27aec9b2f4827
Tor Miller <tor@bizly.com>
2017-01-19 09:20:13 -0500

    added GitHub file back to grid.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,13 +8,11 @@
             <dashboard-info company_logo_src_url="{{ $company_logo_src_url }}" grid="a1:b1"></dashboard-info>
             <current-time grid="c1:d2" dateformat="dddd, MMMM D, YYYY"></current-time>
             <current-weather-conditions grid="e1:f2"></current-weather-conditions>
-            <github-file file-name="README" title="Bizly Dashboard" grid="c3"></github-file>
-
-
             <internet-connection grid="a2:b2"></internet-connection>
             <google-vacation-calendar grid="a3:b8"></google-vacation-calendar>
             <google-company-calendar grid="c3:d8"></google-company-calendar>
             <system-statistics grid="e3:f5"></system-statistics>
+            <github-file file-name="README" title="Bizly Dashboard" grid="g1:h2"></github-file>
             <system-activity grid="i1:j8"></system-activity>
 
     </div>
